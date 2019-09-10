7f1e01690b37b329ae2adca78a43e10a7c80607a
Rajasrilakshmi <rajasrilakshmir@gmail.com>
2019-09-10 11:10:45 +0530

    1D List

--- src/test/java/com/stepdefinition/AddCustomerSteps.java
+++ src/test/java/com/stepdefinition/AddCustomerSteps.java
@@ -1,5 +1,7 @@
 package com.stepdefinition;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 
 
 public class AddCustomerSteps {
@@ -26,12 +29,22 @@ public class AddCustomerSteps {
 
 	@When("user enters all the fields with valid data")
 	public void user_enters_all_the_fields_with_valid_data() {
-	    driver.findElement(By.xpath("//label[@for='done']"));
+	    driver.findElement(By.xpath("//label[@for='done']")).click();
 	    driver.findElement(By.id("fname")).sendKeys("Rajasri");
 	    driver.findElement(By.id("lname")).sendKeys("lakshmi");
 	    driver.findElement(By.id("email")).sendKeys("rajasri@gmail.com");
 	    driver.findElement(By.name("addr")).sendKeys("Chennai");
 	    driver.findElement(By.id("telephoneno")).sendKeys("8734568734");
+	}
+	@When("user enters all the fields with valid data.")
+	public void user_enters_all_the_fields_with_valid_data(DataTable custData) {
+List<String> datas = custData.asList(String.class);
+driver.findElement(By.xpath("//label[@for='done']")).click();
+driver.findElement(By.id("fname")).sendKeys(datas.get(0));
+driver.findElement(By.id("lname")).sendKeys(datas.get(1));
+driver.findElement(By.id("email")).sendKeys(datas.get(2));
+driver.findElement(By.name("addr")).sendKeys(datas.get(3));
+driver.findElement(By.id("telephoneno")).sendKeys(datas.get(4));
 	}
 
 	@When("user clicks on submit button")
--- target/test-classes/Features/AddCustomer.feature
+++ target/test-classes/Features/AddCustomer.feature
@@ -6,3 +6,11 @@ Feature: AddCustomer flow
     When user enters all the fields with valid data
     And user clicks on submit button
     Then user should be displayed customer id is generated
+
+  Scenario: 
+    Given user should be in the telecom home page
+    And user click on add customer button
+    When user enters all the fields with valid data.
+      | Rajasri | lakshmi | rajasri@gmail.com | Pondy | 9845673487 |
+    And user clicks on submit button
+    Then user should be displayed customer id is generated
